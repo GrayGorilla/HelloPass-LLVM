73b9b240435e0aa6d63449f48f69337c3a2fbbe4
Nathan D. Brennan <nbren004@ucr.edu>
2022-03-07 01:53:35 -0800

    fixed liveness build_run automation

--- test/Liveness/run_liveness.sh
+++ test/Liveness/run_liveness.sh
@@ -1,6 +1,6 @@
-cd ../Pass/build/
+cd ../../Pass/build/
 cmake -DCMAKE_BUILD_TYPE=Release ../Transforms/Liveness
 make -j4
-cd ../../test/
+cd ../../test/Liveness/
 clang -S -emit-llvm test$1.c -o test$1.ll
-opt -load ../Pass/build/libLLVMValueNumberingPass.so -enable-new-pm=0 -Liveness test$1.ll
+opt -load ../../Pass/build/libLLVMLivenessPass.so -enable-new-pm=0 -Liveness test$1.ll
